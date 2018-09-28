6e2688cf48d484fe845c886fc142fd74a18c2137
Bogusław Zybert <b.zybert@gmail.com>
2018-09-28 14:18:27 +0200

    done

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,15 @@
 //Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const acceptLanguage = req.headers['accept-language'];
+    console.log('Preferowane języki:', acceptLanguage);
+
+    res.setHeader("Content-Type", "text/html; charset=utf-8")
+    res.end(`Hello, World from back-end!`)
+});
+
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,16 @@
 //Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const acceptLanguage = req.headers['accept-language'];
+    const userAgent = req.headers['user-agent'];
+    console.log('Informacje z przeglądarki:', userAgent);
+
+    res.setHeader("Content-Type", "text/html; charset=utf-8")
+    res.end(`Informacje z przeglądarki: + ${userAgent}`)
+});
+
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
